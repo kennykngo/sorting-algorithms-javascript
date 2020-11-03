c4d1c16a66bc2a711143624e7847a1e274b3888c
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-11-02 19:11:44 -0800

    finished bubbleSort and random num generator

--- bubbleSort.js
+++ bubbleSort.js
@@ -21,8 +21,38 @@
 // const numbers = [8, 5, 6, 9, 3, 1, 4, 2, 7, 10];
 // bubbleSort(numbers);
 
+// using an auxillary variable to hold the value to not overwrite the past value
 function swap(arr, firstIndex, secondIndex) {
-  var temp = arr[firstIndex];
+  var aux = arr[firstIndex];
   arr[firstIndex] = arr[secondIndex];
-  arr[secondIndex] = temp;
+  arr[secondIndex] = aux;
 }
+
+function bubbleSort(arr) {
+  var length = arr.length,
+    i,
+    j;
+
+  for (i = length - 1; i >= 0; i--) {
+    for (j = 0; j <= i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+function randomTen() {
+  let k = 0;
+  let array = [];
+  while (k < 10) {
+    let random = Math.floor(Math.random() * 10);
+    array.push(random);
+    k++;
+  }
+  return array;
+}
+
+randomTen();
+console.log(bubbleSort(randomTen()));
